9d78ca504f6e95727665cd7d906b3affe7171cbe
djaxoqja <163632316+djaxoqja@users.noreply.github.com>
2025-06-11 17:02:17 +0900

    .

--- src/main/java/com/welab/k8s_backend_user/K8sBackendUserApplication.java
+++ src/main/java/com/welab/k8s_backend_user/K8sBackendUserApplication.java
@@ -2,11 +2,14 @@ package com.welab.k8s_backend_user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class K8sBackendUserApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Jenkins Build Trigger");
 		SpringApplication.run(K8sBackendUserApplication.class, args);
 	}
 
